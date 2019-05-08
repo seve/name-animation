e9f5aae6725dff8e6e5bd800efa845e591e5ff02
Severiano Badajoz <sevebadajoz@gmail.com>
2019-05-08 15:20:36 -0700

    Begin project

---
+++ index.js
@@ -0,0 +1,16 @@
+var holder = d3.select("#canvas");
+
+holder.append('line')
+.style('stroke', 'white')
+.style('stroke-width', '16')
+.attr('x1', 0)
+  .attr('y1', 0)
+  .attr('x2', window.outerWidth)
+  .attr('y1', 0);
+holder.append('line')
+.style('stroke', 'black')
+.style('stroke-width', '16')
+.attr('x1', 0)
+  .attr('y1', 16)
+  .attr('x2', window.outerWidth)
+  .attr('y2', 16);
