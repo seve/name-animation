46eb6108f044bca1f644a4af170eef4c69156046
Severiano Badajoz <sevebadajoz@gmail.com>
2019-05-11 22:45:34 -0700

    Generate lines

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-var holder = d3.select("#canvas");
+const holder = d3.select("#canvas");
 
-holder.append('line')
-.style('stroke', 'white')
-.style('stroke-width', '16')
-.attr('x1', 0)
-  .attr('y1', 0)
+const numOfGroups = 3 * window.outerHeight / 16 ;
+
+const lines = []
+
+let y = 0;
+let color
+
+for(let i = 0; i < numOfGroups; i += 1) {
+  switch (i % 3){
+    case 0:
+      color = 'white';
+      break;
+    case 1:
+      color = 'black';
+      break;
+    case 2:
+      color = 'rgba(0,0,0,0)';
+      break;
+    default:
+      break;
+  }
+
+lines.push(holder.append('line')
+  .style('stroke', color)
+  .style('stroke-width', '16')
+  .attr('x1', 0)
+  .attr('y1', y)
   .attr('x2', window.outerWidth)
-  .attr('y1', 0);
-holder.append('line')
-.style('stroke', 'black')
-.style('stroke-width', '16')
-.attr('x1', 0)
-  .attr('y1', 16)
-  .attr('x2', window.outerWidth)
-  .attr('y2', 16);
+  .attr('y2', y));
+
+  y+= 16;
+
+
+}
+
+
 
